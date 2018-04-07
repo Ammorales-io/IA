1b6b787c2ee1c37e65ffd918f96e88a50da37f1c
cdsg25 <c.catwe.95@gmail.com>
2018-04-07 21:25:00 +0200

    Finished exercises 1 and 4.1. Exercise 3 needs to be revised.

--- practica03/2362_p3_10.pl
+++ practica03/2362_p3_10.pl
@@ -1,2 +1,42 @@
-invertlist([], []).
-invertlist([X|R], L) :- invertlist(R, L1), append(L1, [X], L).
+%-----------------------------------------------
+% 
+%     Lab assignment 3: Prolog
+%     LAB GROUP: 2363
+%     Couple: 10
+%     Author 1: Celia San Gregorio Moreno 
+%     Author 2: Álvaro Martínez Morales
+%
+%-----------------------------------------------
+
+%----------------%
+%   Ejercicio 1  %
+%----------------%
+pertenece_m(X, [Y|_]) :- Y \= [_|_], X=Y.
+pertenece_m(X, [L|Rs]) :- pertenece_m(X, L); pertenece_m(X, Rs).
+
+%----------------%
+%   Ejercicio 2  %
+%----------------%
+concatena([], L, L).
+concatena([X|L1], L2, [X|L3]) :- concatena(L1, L2, L3).
+
+invierte([], []).
+invierte([X|R], L) :- invierte(R, L1), concatena(L1, [X], L).
+
+%----------------%
+%   Ejercicio 3  %
+%----------------%
+insert([X-P], [], [X-P]).
+insert([X-P], [A-Q|L1], R) :- P<Q, concatena([X-P], [A-Q|L1], R).
+insert([X-P], [A-Q|L1], [A-Q|L2]) :- insert([X-P], L1, L2).
+
+%------------------%
+%   Ejercicio 4.1  %
+%------------------%
+elem_count(X, [], 0).
+elem_count(X, [X|Ls], C1) :- elem_count(X, Ls, C), C1 is C + 1.
+elem_count(X, [_|Ls], C) :- elem_count(X, Ls, C).
+
+%------------------%
+%   Ejercicio 4.2  %
+%------------------%
