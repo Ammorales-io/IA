64d11beb6d06eaf8cab6f5bc4c4c019f56b8a89b
Amartinez-h <estudiante.defessus@gmail.com>
2018-04-17 19:27:06 +0200

    Corregido el nombre del código de la práctica 3. Memoria estructurada, queda redactar los comentarios.

--- practica03/2363_p3_10.pl
+++ practica03/2363_p3_10.pl
@@ -73,4 +73,5 @@ encode_list([X|RL], [R1|R2], T) :- encode_elem(X, R1, T), encode_list(RL, R2, T)
 %   Ejercicio 8  %
 %----------------%
 dictionary(X) :- X = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z].
+
 encode(L1, L2) :- dictionary(D), list_count(D, L1, RLC), sort_list(RLC, RSL), invierte(RSL, RI), build_tree(RI, T), encode_list(L1, L2, T).
